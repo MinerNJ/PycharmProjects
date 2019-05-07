4796605a59e7bd89e013dd2f38f6d9d7c96b0293
Nick Miner <Nick.Miner18@gmail.com>
2019-05-07 11:40:41 -0400

    working

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -60,6 +60,22 @@
 #         print(meal)
 #         for ingredient in meal:
 #             print(ingredient)
+#
+# my_list = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+# my_iterator = iter(my_list)
+#
+# for i in range(0, len(my_list)):
+#     next_item = next(my_iterator)
+#     print(next_item)
 
+imelda = "More Mayhem", "Imelda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+print(imelda)
+
+title, artist, year, tracks = imelda
+for song in tracks:
+    track, title = song
+    print("\tTrack number {}, Title: {}".format(track, title))
 
 
